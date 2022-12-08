a483c245f85b6bdcc78c0cba790ff4f36da0f643
CINDY <git@github.com:CINDYMUTHON/challenge2.git>
2022-12-08 21:54:06 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,36 +1,71 @@
 
 
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
-let modal = document.getElementById("modal");
-modal.classList.add("hidden")
 
-let likeBtn = document.querySelectorAll("like-glyph");
-likeBtn.addEventListener("click", mimicServerCall)
-let likeBtnArr = Array.from(likeBtn);
-likeBtnArr.forEach((elem)=>{
-  console.log(elem.textContent)
-elem.addEventListener("click", function(){
-  elem.textContent = FULL_HEART;
-  })
-})
 
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+function reverseString(word) {
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
 }
+function isPalindrome(word) {
+  // Write your algorithm here
+    const reversedWord = reverseString(word);
+    if (word === reversedWord) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+/* 
+  Add your pseudocode here
+*/
+  // type in input string
+  //reverse the input string
+  // if the input string is the same as the reverse
+  //   return true
+  // else
+  //   return false
+
+/*
+  Add written explanation of your solution here
+  // create an array from the input string
+  // reverse the array
+  // create a string from the reversed array
+  // return the reversed string
+*/
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+}
+
+module.exports = isPalindrome;
